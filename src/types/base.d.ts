da25767c939a0d1b9c12a401e83ab3b3cfcf1db0
jaywcjlove <398188662@qq.com>
2020-09-27 00:42:06 +0800

    type: Update EventListener.

--- src/types/base.d.ts
+++ src/types/base.d.ts
@@ -177,6 +177,20 @@ declare namespace AMap {
      * @param handler 
      */
     off(event: string, handler: any): void;
+    /**
+     * 判断当前实例是否已经绑定了某个事件回调
+     * @param type 事件类型
+     * @param handler 事件回调
+     * @param context 事件上下文
+     */
+    hasEvents(type: string, handler: any, context: any): boolean;
+    /**
+     * 清除当前实例某一类型的全部事件回调
+     * @param type 事件类型，如果此参数为空，清除实例上的所有绑定的事件回调
+     */
+    clearEvents(type: string): any;
+    /** 模拟触发当前实例的某个事件 */
+    emit(type: string, data: any): any;
   }
   type MapsEvent = {
     /**
