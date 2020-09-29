55d44ec4fc666077bc734882f9211c958481cefd
wangxingkang <156148958@qq.com>
2020-09-29 20:07:34 +0800

    fix(map): 修复地图组件 children 存在DOM元素时警告报错

--- src/Map/index.tsx
+++ src/Map/index.tsx
@@ -14,16 +14,32 @@ export const Map = React.forwardRef<MapProps & { map?: AMap.Map }, MapProps>(({ 
   useEffect(() => setContainer(elmRef.current), [elmRef.current]);
   useImperativeHandle(ref, () => ({ ...props, map, AMap, container: elmRef.current }), [map]);
   const childs = React.Children.toArray(children);
+
+  const renderChildren = () => {
+    return childs.map((child) => {
+      if (child && React.isValidElement(child)) {
+        const type = child.type;
+        /*
+         * 下面两种组件不注入地图相关属性
+         * 1. 明确声明不需要注入的
+         * 2. DOM 元素
+         */
+        if (type.preventAmap || (typeof type === 'string')) {
+          return child
+        }
+        return React.cloneElement(child, {
+          ...child.props, AMap, map, container,
+        });
+      }
+      return child
+    })
+  }
+
   return (
     <Fragment>
       <div ref={elmRef} className={className} style={{ fontSize: 1, height: '100%', ...style}} />
       {AMap && map && typeof children === 'function' && children({ AMap, map, container })}
-      {AMap && map && childs.map((child) => {
-        if (!React.isValidElement(child)) return;
-        return React.cloneElement(child, {
-          ...child.props, AMap, map, container,
-        });
-      })}
+      {AMap && map && renderChildren()}
     </Fragment>
   );
 });
