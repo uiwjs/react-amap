3ce3036ae7708c791bf99512d9fec34c0a7c2c6a
jaywcjlove <398188662@qq.com>
2020-10-08 21:10:56 +0800

    type: Update src/types/index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,5 +5,7 @@
 /// <reference path="./base.d.ts" />
 /// <reference path="./core.d.ts" />
 /// <reference path="./control.d.ts" />
+/// <reference path="./district.d.ts" />
 /// <reference path="./maplayer.d.ts" />
 /// <reference path="./overlay.d.ts" />
+/// <reference path="./services.d.ts" />
