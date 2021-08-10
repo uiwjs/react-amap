811215bbce25a59d8ca4ecfca902844758088dd2
jaywcjlove <398188662@qq.com>
2021-08-10 19:20:28 +0800

    website(deps): Update dependency.

--- website/components/Markdown/index.tsx
+++ website/components/Markdown/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import { Component, Fragment } from 'react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import rehypeAttr from 'rehype-attr';
 import Code, { CodeProps } from './Code';
@@ -20,28 +20,6 @@ const getCodeStr = (data: any[] = [], code: string = '') => {
   });
   return code;
 };
-
-/**
- * 代码注释参数
- *
- * ```md
- * <!--rehype:bgWhite=true&codeSandbox=true&codePen=true-->
- * ```
- * 参数用英文逗号隔开
- *
- * - `bgWhite` 设置代码预览背景白色，否则为格子背景。
- * - `noCode` 不显示代码编辑器。
- * - `noPreview` 不显示代码预览效果。
- * - `noScroll` 预览区域不显示滚动条。
- * - `codePen` 显示 Codepen 按钮，要特别注意 `包导入的问题`，实例中的 `import` 主要用于 Codepen 使用。
- */
-export interface OptionsMarkdown {
-  bgWhite?: boolean;
-  noCode?: boolean;
-  noPreview?: boolean;
-  noScroll?: boolean;
-  codePen?: boolean;
-}
 
 export default class Markdown extends Component<MarkdownProps, MarkdownState> {
   constructor(props: MarkdownProps) {
@@ -69,14 +47,21 @@ export default class Markdown extends Component<MarkdownProps, MarkdownState> {
           style={{ padding: '20px 26px' }}
           source={this.state.mdStr}
           className={styles.markdown}
-          rehypePlugins={[rehypeAttr]}
+          rehypePlugins={[rehypeAttr as any]}
           components={{
             /**
-             * bgWhite 设置代码预览背景白色，否则为格子背景。
-             * noCode 不显示代码编辑器。
-             * noPreview 不显示代码预览效果。
-             * noScroll 预览区域不显示滚动条。
-             * codePen 显示 Codepen 按钮，要特别注意 包导入的问题，实例中的 import 主要用于 Codepen 使用。
+             * 代码注释参数
+             *
+             * ```md
+             * <!--rehype:bgWhite=true&codeSandbox=true&codePen=true-->
+             * ```
+             * 参数用英文逗号隔开
+             *
+             * - `bgWhite` 设置代码预览背景白色，否则为格子背景。
+             * - `noCode` 不显示代码编辑器。
+             * - `noPreview` 不显示代码预览效果。
+             * - `noScroll` 预览区域不显示滚动条。
+             * - `codePen` 显示 Codepen 按钮，要特别注意 `包导入的问题`，实例中的 `import` 主要用于 Codepen 使用。
              */
             code: ({ 'data-config': config, inline, node, ...props }) => {
               if (config) {
