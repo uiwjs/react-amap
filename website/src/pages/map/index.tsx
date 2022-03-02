64e50c73f6d11e8c429c9f6b586d00c74ef7c4c6
jaywcjlove <398188662@qq.com>
2022-03-03 00:50:55 +0800

    website(Map): fix example issue.

--- website/src/pages/map/index.tsx
+++ website/src/pages/map/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom';
 import React, { Fragment, useRef, useEffect, useState } from 'react';
-import { Map, useMapContext, APILoader, Polyline } from '@uiw/react-amap';
+import * as map from '@uiw/react-amap';
 import Markdown from '../../components/Markdown';
 
 export default class Page extends Markdown {
   editorUrl = '/packages/map/README.md';
-  dependencies = { React, ReactDOM, Fragment, useRef, useEffect, useState, Polyline, Map, useMapContext, APILoader };
+  dependencies = { React, ReactDOM, Fragment, useRef, useEffect, useState, ...map };
   getMdStr = () => import('@uiw/react-amap-map/README.md');
 }
