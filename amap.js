f9b15cbbd84911392e45f5515900dfc2424c7ab2
jaywcjlove <jaywcjlove@users.noreply.github.com>
2023-07-12 12:28:15 +0000

    v5.0.5 fix(polygon): ignore error when map unmounted (#321) 8f601e738d994f4dab54fe634c9836ffbf59d3f9

--- amap.js
+++ amap.js
@@ -1826,15 +1826,18 @@ var usePolygon = function usePolygon(props) {
       setPolygon(instance);
       return () => {
         if (instance) {
-          map && map.remove(instance);
+          try {
+            map && map.remove(instance);
+          } catch (e) {}
           // if (AMap.v) {
           //   map && map.remove(instance);
           // } else {
           //   // 暂不使用这个 API，这个不兼容 v1.4.xx，改用 map.remove API
           //   map && map.removeLayer(instance);
           // }
-          setPolygon(undefined);
         }
+
+        setPolygon(undefined);
       };
     }
   }, [map]);
