d8ab78a68947248a945306fd72b385f61f04810c
小弟调调 <398188662@qq.com>
2025-05-22 18:53:42 +0800

    test: update test case.

--- packages/amap/__tests__/index.test.tsx
+++ packages/amap/__tests__/index.test.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { Map, useMap } from '../src';
 import * as apis from '../src';
 
 describe('<Map />', () => {
   it('Map test case', () => {
-    expect(Object.keys(apis).length).toEqual(66);
+    expect(Object.keys(apis).length).toEqual(67);
     expect(typeof Map).toEqual('object');
     expect(typeof useMap).toEqual('function');
     const component = TestRenderer.create(<Map />);
